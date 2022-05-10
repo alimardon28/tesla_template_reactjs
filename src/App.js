2224c412f5a8056b0ac399944c40d90ad45e115e
alimardon28 <doniyorovalimardon369@gmail.com>
2022-05-10 16:07:52 +0500

    finished model3 pages , finished responsive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Header/Header';
+import Model3 from './Pages/Model3/Model3';
 
 function App() {
   return (
     <div className="App">
          <Header/>
+         <Model3/>
     </div>
   );
 }
