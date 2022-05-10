cd734c57f421b0eaade5952f569ecf4aaad5e633
alimardon28 <doniyorovalimardon369@gmail.com>
2022-05-10 16:15:26 +0500

    finished models pages and responsive

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import './App.css';
-import Header from './Components/Header/Header';
-import Model3 from './Pages/Model3/Model3';
-import ModelY from './Pages/ModelY/ModelY';
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Model3 from "./Pages/Model3/Model3";
+import ModelS from "./Pages/ModelS/ModelS";
+import ModelY from "./Pages/ModelY/ModelY";
 
 function App() {
   return (
     <div className="App">
-         <Header/>
-         <Model3/>
-         <ModelY/>
+      <Header />
+      <Model3 />
+      <ModelY />
+      <ModelS />
     </div>
   );
 }
--- src/Pages/ModelS/ModelS.jsx
+++ src/Pages/ModelS/ModelS.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import '../ModelS/_ModelS.scss';
+import React from "react";
+import "../ModelS/_ModelS.scss";
 
 const ModelS = () => {
-    return (
-      <div className="models">
-        
+  return (
+    <div className="models">
+      <div className="container models__container">
+        <h1 className="models__heading">Model S</h1>
+        <p className="models__desc">
+          {" "}
+          Order Online for{" "}
+          <a href="#" className="models__link">
+            Touchless Delivery
+          </a>
+        </p>
+        <div className="models__buttons">
+          <button className="models__buttons_btn">CUSTOM ORDER</button>
+          <button className="models__buttons_btn">EXISTING INVENTORY</button>
+        </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default ModelS;
